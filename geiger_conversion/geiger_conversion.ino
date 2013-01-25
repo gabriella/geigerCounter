0b2cfc2468e304d03eaa5f1a07e551020316274a
gabriella levine <gabriella.levine@gmail.com>
2013-01-25 19:27:52 +0900

    geiger counter code 1 for sparkfun geiger counter

---
+++ geiger_conversion/geiger_conversion.ino
@@ -0,0 +1,124 @@
+//
+//  Geiger Counter Firmware
+//  
+//  by gabriella levine
+//  Based on the work of Aaron Weiss
+//
+
+
+//Yup. You just set your arduino's input pin to read a pin state change and count that. In parallel, have a timer running and after 6
+//seconds, display the count variable.
+
+
+
+#include <Metro.h> //Include Metro library
+
+#include <TimerOne.h>
+
+int CPM = 0;
+long Baud      = 9600;
+int  LEDPin    = 5;      // Status LED blinks for each count
+long CountTime = 1000000; // 1 second, in microseconds
+long interval = 10000;//1 minute long change this from 1 second
+long prevTime = 0;
+unsigned long currentTime;
+Metro CPMMetro = Metro(10000);
+
+int countState = 0;
+int prevCountState = 0;
+int counter = 0;
+
+volatile long counts = 0;
+
+void incrementCount(void) {
+  counts++;
+  digitalWrite(LEDPin, HIGH);
+}
+
+void printCount(void) {
+  noInterrupts();
+  Serial.print("millis:");
+  Serial.print(millis());
+  Serial.print(";");
+  Serial.print("counts");
+  Serial.print(counts);
+  
+  
+//  Serial.print("counter = ");
+//  Serial.println(counter);
+//  Serial.print("countState = ");
+//  Serial.println(countState);
+//  Serial.print("prevCountState = ");
+//  Serial.println(prevCountState);
+
+
+if(counts != prevCountState){
+  Serial.println("different");
+ if(counts != 0){
+   counter++;
+  Serial.print("counter number = ");
+  Serial.println(counter);
+ } 
+}
+
+prevCountState = counts;
+
+  
+  
+  counts = 0;
+  
+  
+  
+  
+  
+  
+  interrupts();
+}
+
+void setup() {
+  Serial.begin(Baud);
+  noInterrupts();
+  attachInterrupt(INT0, incrementCount, FALLING);
+  Timer1.initialize(CountTime);
+  Timer1.attachInterrupt(printCount);
+
+  pinMode(LEDPin, OUTPUT);
+  interrupts();
+  delay(6000);
+
+
+}
+
+void loop() {
+
+
+
+  digitalWrite(LEDPin, LOW);
+  if(CPMMetro.check()==1){
+    Serial.print("Startover");
+    counter = 0;
+    
+    Serial.print("millis");
+    Serial.println(millis());
+    Serial.print("minutes");
+    
+    unsigned long minutes = millis()/1000;
+    Serial.println(minutes);
+  }
+
+  delay(30);
+}
+//
+//void resetTime(){
+//   currentTime= millis();
+// if(currentTime-prevTime>interval){
+//   prevTime = currentTime;
+//     Serial.print(prevTime);
+//
+// } 
+//
+//  
+//}
+
+
+
